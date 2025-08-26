8a2829cb36a1b28d7661044bfc4ef3b13f07c15d
chiranuvathn <chiranuvathn@gmail.com>
2025-08-26 22:49:18 +0700

    - display 'Book' model in HTML template via 'book_list' view

--- web_pages/urls.py
+++ web_pages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='home'),
+    path('books/', views.book_list, name='book_list'),
 ]
--- web_pages/views.py
+++ web_pages/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Book
 
 def homepage(request):
     return HttpResponse('Welcome to the Library!')
+
+def book_list(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    
+    return render(request, 'pages/book_list.html', context)
