4321fbb4ced729828c4cef7757e37d282c74cc27
chiranuvathn <chiranuvathn@gmail.com>
2025-08-27 19:49:09 +0700

    - create 'BookForm' based off 'Book' model to handle books related inputs

---
+++ web_pages/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from .models import Book
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = [
+            "title",
+            "author",
+            "published_date"
+        ]
