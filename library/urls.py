65064a39f8dd2d4547edd09b02ce096a5ed1a450
GearChira <chiranuvathn@gmail.com>
2025-09-20 11:12:52 +0700

    - add login feature to the site

--- library/urls.py
+++ library/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web_pages.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
