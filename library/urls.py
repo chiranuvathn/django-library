a82f0ae075385b96f252661114d397b661e7edf1
chiranuvathn <chiranuvathn@gmail.com>
2025-09-26 22:42:37 +0700

    - add signup feature to the site

--- library/urls.py
+++ library/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('web_pages.urls')),
     path('', TemplateView.as_view(template_name='home.html'),name='home'),
+    path('accounts/signup/', views.sign_up, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
