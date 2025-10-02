151bcd95034bb63a86550504d78c9594141323e5
chiranuvathn <chiranuvathn@gmail.com>
2025-10-02 22:32:12 +0700

    - add 'user' field to 'Book' model for storing ownership

--- web_pages/models.py
+++ web_pages/models.py
@@ -1,9 +1,14 @@
+from django.contrib.auth.models import User
+
 from django.db import models
 
 class Book (models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+        null=True, blank=True)
 
     def __str__(self):
         return f"{self.title} by {self.author}"
