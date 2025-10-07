a312732127b37da4b6546c805a784894009b6573
GearChira <chiranuvathn@gmail.com>
2025-10-07 22:23:11 +0700

    - add 'edit_book' and 'delete_book' restriction based on user

--- web_pages/views.py
+++ web_pages/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseForbidden
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
@@ -67,7 +67,10 @@ def add_book(request):
 
 @login_required
 def edit_book(request, id):
-    book = get_object_or_404(Book, pk=id)
+    try:
+        book = get_object_or_404(Book, pk=id, user=request.user)
+    except Http404:
+        return HttpResponseForbidden("Access Denied. You do not have permission to edit this book.")
     
     if request.method == 'POST':
         # package form inputs and update 'Book' model by id
@@ -91,6 +94,9 @@ def edit_book(request, id):
 
 @login_required
 def delete_book(request, id):
-    book = get_object_or_404(Book, pk=id)
+    try:
+        book = get_object_or_404(Book, pk=id, user=request.user)
+    except Http404:
+        return HttpResponseForbidden("Access Denied. You do not have permission to delete this book.")
     book.delete()
     return redirect('book_list')
