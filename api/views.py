60a7c131c06a9f1b952b17fa3dd0e79bbf44ff7f
chiranuvathn <chiranuvathn@gmail.com>
2025-12-03 21:59:27 +0700

    - create a viewsets for API endpoints

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from web_pages.models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
